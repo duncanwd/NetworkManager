fc238953a00d0e730f6883827086d9bda58ebe1c
Thomas Haller <thaller@redhat.com>
2022-02-11 17:18:17 +0100

    glib-aux: add "name_ptr" union field to NMUtilsNamedValue

--- src/libnm-glib-aux/nm-shared-utils.h
+++ src/libnm-glib-aux/nm-shared-utils.h
@@ -1961,6 +1961,7 @@ typedef struct {
         NMUtilsNamedEntry named_entry;
         const char       *name;
         char             *name_mutable;
+        gpointer          name_ptr;
     };
     union {
         const char *value_str;
