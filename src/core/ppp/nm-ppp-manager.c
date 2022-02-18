4d165a64275746b5d9cab5dc94d6f27d534438ac
Thomas Haller <thaller@redhat.com>
2022-02-18 13:36:44 +0100

    core: use NM_SHUTDOWN_TIMEOUT_1500_MSEC
    At some places we scheduled a timeout in NM_SHUTDOWN_TIMEOUT_MAX_MSEC.
    There, we want to make sure that we don't take longer than
    NM_SHUTDOWN_TIMEOUT_MAX_MSEC. But this leaves the actual wait time
    unspecified.
    
    Those callers don't want to wait an undefined time. They really should
    be clear about how long they wait. Hence, use NM_SHUTDOWN_TIMEOUT_1500_MSEC
    which makes it clear this is 1500 msec and not longer than NM_SHUTDOWN_TIMEOUT_MAX_MSEC.

--- src/core/ppp/nm-ppp-manager.c
+++ src/core/ppp/nm-ppp-manager.c
@@ -1246,7 +1246,7 @@ _ppp_manager_stop(NMPPPManager            *self,
                               SIGTERM,
                               LOGD_PPP,
                               "pppd",
-                              NM_SHUTDOWN_TIMEOUT_MAX_MSEC,
+                              NM_SHUTDOWN_TIMEOUT_1500_MSEC,
                               _stop_child_cb,
                               handle);
 
